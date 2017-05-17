1e3714de3fd29743d1d2ff776bebbb677dd20444
DaMatrix <joey.rabil@gmail.com>
2017-05-17 09:34:20 +0200

    push a missing file

---
+++ src/main/java/net/daporkchop/pepsimod/gui/misc/GuiButtonTooBeeTooTee.java
@@ -0,0 +1,40 @@
+package net.daporkchop.pepsimod.gui.misc;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+
+public class GuiButtonTooBeeTooTee extends GuiButton {
+    private ResourceLocation location;
+
+    public GuiButtonTooBeeTooTee(int buttonId, int x, int y, int widthIn, int heightIn) {
+        super(buttonId, x, y, widthIn, heightIn, "");
+        this.width = 20;
+        this.height = 20;
+        this.enabled = true;
+        this.visible = true;
+        this.id = buttonId;
+        this.xPosition = x;
+        this.yPosition = y;
+        this.width = widthIn;
+        this.height = heightIn;
+        this.location = new ResourceLocation("textures/gui/pepsibuttons.png");
+    }
+
+
+    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        if (this.visible) {
+            mc.getTextureManager().bindTexture(location);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            this.hovered = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+            int k = this.getHoverState(this.hovered);
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+            GlStateManager.blendFunc(770, 771);
+            this.drawTexturedModalRect(this.xPosition, this.yPosition, 20, (hovered ? 20 : 0), this.width, this.height);
+            this.drawTexturedModalRect(this.xPosition + this.width, this.yPosition, 200 - this.width, k * 20, this.width, this.height);
+            this.mouseDragged(mc, mouseX, mouseY);
+        }
+    }
+}
