a402b427358a493a5fb211e88c1cc3146031f73f
DaMatrix <joey.rabil@gmail.com>
2017-12-13 18:29:41 +0100

    remove a lot of uneeded overwrite code

--- src/main/java/net/daporkchop/pepsimod/mixin/client/entity/MixinEntityPlayerSP.java
+++ src/main/java/net/daporkchop/pepsimod/mixin/client/entity/MixinEntityPlayerSP.java
@@ -21,53 +21,30 @@ import net.daporkchop.pepsimod.module.impl.movement.NoSlowdownMod;
 import net.daporkchop.pepsimod.module.impl.movement.StepMod;
 import net.daporkchop.pepsimod.the.wurst.pkg.name.RotationUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.entity.IJumpingMount;
-import net.minecraft.init.Items;
-import net.minecraft.init.MobEffects;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.ItemElytra;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.MovementInput;
 import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityPlayerSP.class)
 public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
     @Shadow
-    public final NetHandlerPlayClient connection;
-    @Shadow
-    public int sprintingTicksLeft;
-    @Shadow
-    public float timeInPortal;
-    @Shadow
-    public float prevTimeInPortal;
-    @Shadow
-    public MovementInput movementInput;
+    @Final
+    public NetHandlerPlayClient connection;
     @Shadow
     protected Minecraft mc;
-    @Shadow
-    protected int sprintToggleTimer;
-    @Shadow
-    private float horseJumpPower;
-    @Shadow
-    private int autoJumpTime;
-    @Shadow
-    private int horseJumpPowerCounter;
-    @Shadow
-    private boolean wasFallFlying;
     @Shadow
     private double lastReportedPosX;
     @Shadow
@@ -91,199 +68,18 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
 
     public MixinEntityPlayerSP() {
         super(null, null);
-        connection = null;
     }
 
-    @Overwrite
-    public void onLivingUpdate() {
-        ++this.sprintingTicksLeft;
-
-        if (this.sprintToggleTimer > 0) {
-            --this.sprintToggleTimer;
-        }
-
-        this.prevTimeInPortal = this.timeInPortal;
-
-        if (this.inPortal) {
-            if (this.mc.currentScreen != null && !this.mc.currentScreen.doesGuiPauseGame()) {
-                this.mc.displayGuiScreen(null);
-            }
-
-            if (this.timeInPortal == 0.0F) {
-                this.mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.BLOCK_PORTAL_TRIGGER, this.rand.nextFloat() * 0.4F + 0.8F));
-            }
-
-            this.timeInPortal += 0.0125F;
-
-            if (this.timeInPortal >= 1.0F) {
-                this.timeInPortal = 1.0F;
-            }
-
-            this.inPortal = false;
-        } else if (this.isPotionActive(MobEffects.NAUSEA) && this.getActivePotionEffect(MobEffects.NAUSEA).getDuration() > 60) {
-            this.timeInPortal += 0.006666667F;
-
-            if (this.timeInPortal > 1.0F) {
-                this.timeInPortal = 1.0F;
-            }
-        } else {
-            if (this.timeInPortal > 0.0F) {
-                this.timeInPortal -= 0.05F;
-            }
-
-            if (this.timeInPortal < 0.0F) {
-                this.timeInPortal = 0.0F;
-            }
-        }
-
-        if (this.timeUntilPortal > 0) {
-            --this.timeUntilPortal;
-        }
-
-        boolean flag = this.movementInput.jump;
-        boolean flag1 = this.movementInput.sneak;
-        float f = 0.8F;
-        boolean flag2 = this.movementInput.moveForward >= 0.8F;
-        this.movementInput.updatePlayerMoveState();
-        this.mc.getTutorial().handleMovement(this.movementInput);
-
-        if (this.isHandActive() && !this.isRiding() && !NoSlowdownMod.INSTANCE.state.enabled) {
-            this.movementInput.moveStrafe *= 0.2F;
-            this.movementInput.moveForward *= 0.2F;
-            this.sprintToggleTimer = 0;
-        }
-
-        boolean flag3 = false;
-
-        if (this.autoJumpTime > 0) {
-            --this.autoJumpTime;
-            flag3 = true;
-            this.movementInput.jump = true;
-        }
-
-        AxisAlignedBB axisalignedbb = this.getEntityBoundingBox();
-        net.minecraftforge.client.event.PlayerSPPushOutOfBlocksEvent event = new net.minecraftforge.client.event.PlayerSPPushOutOfBlocksEvent(this, axisalignedbb);
-        if (!net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(event)) {
-            axisalignedbb = event.getEntityBoundingBox();
-            this.pushOutOfBlocks(this.posX - (double) this.width * 0.35D, axisalignedbb.minY + 0.5D, this.posZ + (double) this.width * 0.35D);
-            this.pushOutOfBlocks(this.posX - (double) this.width * 0.35D, axisalignedbb.minY + 0.5D, this.posZ - (double) this.width * 0.35D);
-            this.pushOutOfBlocks(this.posX + (double) this.width * 0.35D, axisalignedbb.minY + 0.5D, this.posZ - (double) this.width * 0.35D);
-            this.pushOutOfBlocks(this.posX + (double) this.width * 0.35D, axisalignedbb.minY + 0.5D, this.posZ + (double) this.width * 0.35D);
-        }
-        boolean flag4 = (float) this.getFoodStats().getFoodLevel() > 6.0F || this.capabilities.allowFlying;
-
-        if (this.onGround && !flag1 && !flag2 && this.movementInput.moveForward >= 0.8F && !this.isSprinting() && flag4 && !this.isHandActive() && !this.isPotionActive(MobEffects.BLINDNESS)) {
-            if (this.sprintToggleTimer <= 0 && !this.mc.gameSettings.keyBindSprint.isKeyDown()) {
-                this.sprintToggleTimer = 7;
-            } else {
-                this.setSprinting(true);
-            }
-        }
-
-        if (!this.isSprinting() && this.movementInput.moveForward >= 0.8F && flag4 && !this.isHandActive() && !this.isPotionActive(MobEffects.BLINDNESS) && this.mc.gameSettings.keyBindSprint.isKeyDown()) {
-            this.setSprinting(true);
-        }
-
-        if (this.isSprinting() && (this.movementInput.moveForward < 0.8F || this.collidedHorizontally || !flag4)) {
-            this.setSprinting(false);
-        }
-
-        if (this.capabilities.allowFlying) {
-            if (this.mc.playerController.isSpectatorMode()) {
-                if (!this.capabilities.isFlying) {
-                    this.capabilities.isFlying = true;
-                    this.sendPlayerAbilities();
-                }
-            } else if (!flag && this.movementInput.jump && !flag3) {
-                if (this.flyToggleTimer == 0) {
-                    this.flyToggleTimer = 7;
-                } else {
-                    this.capabilities.isFlying = !this.capabilities.isFlying;
-                    this.sendPlayerAbilities();
-                    this.flyToggleTimer = 0;
-                }
-            }
-        }
-
-        if (this.movementInput.jump && !flag && !this.onGround && this.motionY < 0.0D && !this.isElytraFlying() && !this.capabilities.isFlying) {
-            ItemStack itemstack = this.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
-
-            if (itemstack.getItem() == Items.ELYTRA && ItemElytra.isUsable(itemstack)) {
-                this.connection.sendPacket(new CPacketEntityAction(this, CPacketEntityAction.Action.START_FALL_FLYING));
-            }
-        }
-
-        this.wasFallFlying = this.isElytraFlying();
-
-        if (this.capabilities.isFlying && this.isCurrentViewEntity()) {
-            if (this.movementInput.sneak) {
-                this.movementInput.moveStrafe = (float) ((double) this.movementInput.moveStrafe / 0.3D);
-                this.movementInput.moveForward = (float) ((double) this.movementInput.moveForward / 0.3D);
-                this.motionY -= (double) (this.capabilities.getFlySpeed() * 3.0F);
-            }
-
-            if (this.movementInput.jump) {
-                this.motionY += (double) (this.capabilities.getFlySpeed() * 3.0F);
-            }
-        }
-
-        if (this.isRidingHorse()) {
-            IJumpingMount ijumpingmount = (IJumpingMount) this.getRidingEntity();
-
-            if (this.horseJumpPowerCounter < 0) {
-                ++this.horseJumpPowerCounter;
-
-                if (this.horseJumpPowerCounter == 0) {
-                    this.horseJumpPower = 0.0F;
-                }
-            }
-
-            if (flag && !this.movementInput.jump) {
-                this.horseJumpPowerCounter = -10;
-                ijumpingmount.setJumpPower(MathHelper.floor(this.getHorseJumpPower() * 100.0F));
-                this.sendHorseJump();
-            } else if (!flag && this.movementInput.jump) {
-                this.horseJumpPowerCounter = 0;
-                this.horseJumpPower = 0.0F;
-            } else if (flag) {
-                ++this.horseJumpPowerCounter;
-
-                if (this.horseJumpPowerCounter < 10) {
-                    this.horseJumpPower = (float) this.horseJumpPowerCounter * 0.1F;
-                } else {
-                    this.horseJumpPower = 0.8F + 2.0F / (float) (this.horseJumpPowerCounter - 9) * 0.1F;
-                }
-            }
-        } else {
-            this.horseJumpPower = 0.0F;
-        }
-
-        super.onLivingUpdate();
-
-        if (this.onGround && this.capabilities.isFlying && !this.mc.playerController.isSpectatorMode()) {
-            this.capabilities.isFlying = false;
-            this.sendPlayerAbilities();
-        }
-    }
-
-    @Shadow
-    public boolean isRidingHorse() {
-        return true;
+    @Redirect(method = "onLivingUpdate",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/entity/Entity;isRiding()Z"))
+    public boolean fixNoSlowdown(Entity entity) {
+        return entity.isRiding() && NoSlowdownMod.INSTANCE.state.enabled;
     }
 
     @Shadow
     protected boolean isCurrentViewEntity() {
         return false;
-    }
-
-    @Shadow
-    protected void sendHorseJump() {
-
-    }
-
-    @Shadow
-    public float getHorseJumpPower() {
-        return 0.0f;
     }
 
     @Inject(method = "isAutoJumpEnabled", at = @At("HEAD"), cancellable = true)
