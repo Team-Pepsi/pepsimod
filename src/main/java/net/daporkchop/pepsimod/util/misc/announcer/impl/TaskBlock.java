70ca4bfb66b58b6ab59536ee3a79ca786a9e7b27
DaMatrix <joey.rabil@gmail.com>
2019-05-04 00:12:04 +0200

    make announcer not crash the game
    i'm a twat

--- src/main/java/net/daporkchop/pepsimod/util/misc/announcer/impl/TaskBlock.java
+++ src/main/java/net/daporkchop/pepsimod/util/misc/announcer/impl/TaskBlock.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from the Wizardry License
  *
- * Copyright (c) 2017-2018 DaPorkchop_
+ * Copyright (c) 2017-2019 DaPorkchop_
  *
  * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it.
  * Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
@@ -31,6 +31,6 @@ public class TaskBlock extends QueuedTask {
     }
 
     public String getMessage() {
-        return MessagePrefixes.getMessage(this.type, this.block.getLocalizedName(), String.valueOf(this.count));
+        return MessagePrefixes.getMessage(this.type, this.block.getLocalizedName(), this.count);
     }
 }
